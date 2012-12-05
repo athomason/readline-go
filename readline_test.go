faccaf3f077833bc99f61efa4abb13fb22f20bb0
Adam Thomason <ad@mthomason.net>
2012-12-04 23:26:38 -0800

    Update to work with current Go

--- readline_test.go
+++ readline_test.go
@@ -4,16 +4,18 @@ package readline
 import "testing"
 
 func TestReadline(t *testing.T) {
-	prompt := "by your command> ";
+	prompt := "by your command> "
 
 	//loop until ReadLine returns nil (signalling EOF)
-	L: for {
+L:
+	for {
 		switch result := ReadLine(&prompt); true {
-		case result == nil: break L //exit loop
+		case result == nil:
+			break L //exit loop
 
 		case *result != "": //ignore blank lines
-			println(*result);
-			AddHistory(*result); //allow user to recall this line
+			println(*result)
+			AddHistory(*result) //allow user to recall this line
 		}
 	}
 }
